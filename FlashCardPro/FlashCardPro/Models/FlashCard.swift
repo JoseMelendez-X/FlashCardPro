dd1cb0941f89c0b05fab3691ba801861e9620206
Jose Melendez <Josem_melendez@icloud.com>
2018-04-02 19:55:25 -0400

    defined basic models

---
+++ FlashCardPro/FlashCardPro/Models/FlashCard.swift
@@ -0,0 +1,18 @@
+//
+//  FlashCard.swift
+//  FlashCardPro
+//
+//  Created by Jose Melendez on 4/2/18.
+//  Copyright © 2018 Jose Melendez. All rights reserved.
+//
+
+import Foundation
+
+struct FlashCard {
+    
+    //MARK: - Properties
+    var isFrontFacing = false
+    var answer: String
+    var question: String
+   
+}
---
+++ FlashCardPro/FlashCardPro/Models/FlashCardDeck.swift
@@ -0,0 +1,24 @@
+//
+//  FlashCardDeck.swift
+//  FlashCardPro
+//
+//  Created by Jose Melendez on 4/2/18.
+//  Copyright © 2018 Jose Melendez. All rights reserved.
+//
+
+import Foundation
+
+struct  FlashCardDeck {
+    
+    //Mark: - Properties
+    
+    //array of flashcards
+    var flashCards = [FlashCard]()
+    
+    //MARK: - Functions
+    
+    //remove flashcards
+    
+    //add flashcards
+}
+
