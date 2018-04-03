966327582e842d3ac3d62584eb9bce22f92d767e
Jose Melendez <Josem_melendez@icloud.com>
2018-04-03 13:25:39 -0400

    Designed the CreateNewDeckVC

--- FlashCardPro/FlashCardPro/Controllers/CreateNewDeckViewController.swift
+++ FlashCardPro/FlashCardPro/Controllers/CreateNewDeckViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import TextFieldEffects
 
 class CreateNewDeckViewController: UIViewController {
-
+    
+    
+    //MARK: - IB Outlets
+    @IBOutlet weak var flashCardTitleTextField: HoshiTextField!
+    
+    @IBOutlet weak var doneButton: UIButton!{
+        didSet {
+            //makes button edges rounded
+            doneButton.layer.cornerRadius = 12.0
+        }
+    }
+    
     //MARK: - IB Actions
     
+    @IBAction func XButtonTapped(_ sender: UIButton) {
+        
+        //dimiss self
+        self.dismiss(animated: true, completion: nil)
+    }
     //Create a new deck on flashCardDecks view controller and dismiss itself
     @IBAction func doneButtonTapped(_ sender: UIButton) {
         
-        //dimiss itself
+        //dimiss self
         self.dismiss(animated: true, completion: nil)
     }
 
