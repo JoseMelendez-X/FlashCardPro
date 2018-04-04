93ef2f61f8f27cd49f1b74197738942083fa01fd
Jose Melendez <Josem_melendez@icloud.com>
2018-04-04 19:31:59 -0400

    User can now see one flashCard from the deck

--- FlashCardPro/FlashCardPro/Controllers/FlashCardsViewController.swift
+++ FlashCardPro/FlashCardPro/Controllers/FlashCardsViewController.swift
@@ -7,13 +7,52 @@
 //
 
 import UIKit
+import RealmSwift
 
 class FlashCardsViewController: UIViewController {
 
     //MARK: - Properties
     var flashCardDeck: FlashCardDeck?
+    var flashCards: List<FlashCard>?
+    var isAnswerViewFaceUp = true
     
+    //MARK: - IB Outlets
+    @IBOutlet weak var emptyDataView: UIView!
+    @IBOutlet weak var questionView: UIView!
+    @IBOutlet weak var answerView: UIView!
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var flashCardView: UIView!
+    @IBOutlet weak var questionTextView: UITextView!
+    @IBOutlet weak var answerTextView: UITextView!
     
+    //MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        flashCards = flashCardDeck?.flashCards
+        
+        questionTextView.text = flashCards?.first?.question
+        answerTextView.text = flashCards?.first?.answer
+
+        if flashCardDeck?.flashCards.count == 0 {
+            mainView.isHidden = true
+            flashCardView.isHidden = true
+        }
+    }
+    
+    //MARK: IB Actions
+    @IBAction func flipButtonTapped(_ sender: UIButton) {
+        
+        isAnswerViewFaceUp = !isAnswerViewFaceUp
+        
+        if isAnswerViewFaceUp {
+              UIView.transition(from: answerView, to: questionView, duration: 1.0, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: nil)
+        } else {
+            
+            UIView.transition(from: questionView, to: answerView, duration: 1.0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        }
+      
+    }
     //MARK: - Functions
     
     //Pass flashCard object to the next view controller
