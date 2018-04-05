f655ec97b64f5fc968d5b6b8fcf066897aaf07aa
Jose Melendez <Josem_melendez@icloud.com>
2018-04-05 00:26:35 -0400

    User can show different careds by selecting the buttons

--- FlashCardPro/FlashCardPro/Controllers/FlashCardsViewController.swift
+++ FlashCardPro/FlashCardPro/Controllers/FlashCardsViewController.swift
@@ -15,12 +15,14 @@ class FlashCardsViewController: UIViewController {
     var flashCardDeck: FlashCardDeck?
     var flashCards: List<FlashCard>?
     var isAnswerViewFaceUp = true
+    var count = 0
     
     //MARK: - IB Outlets
     @IBOutlet weak var emptyDataView: UIView!
     @IBOutlet weak var questionView: UIView!
     @IBOutlet weak var answerView: UIView!
     @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var questionAnswerLabel: UILabel!
     @IBOutlet weak var flashCardView: UIView!
     @IBOutlet weak var questionTextView: UITextView!
     @IBOutlet weak var answerTextView: UITextView!
@@ -29,14 +31,22 @@ class FlashCardsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        flashCards = flashCardDeck?.flashCards
-        
-        questionTextView.text = flashCards?.first?.question
-        answerTextView.text = flashCards?.first?.answer
-
         if flashCardDeck?.flashCards.count == 0 {
+            
             mainView.isHidden = true
             flashCardView.isHidden = true
+            
+        } else {
+            
+            flashCards = flashCardDeck?.flashCards
+            
+            questionTextView.text = flashCards![count].question
+            answerTextView.text = flashCards![count].answer
+            
+            flashCardView.layer.cornerRadius = 15.0
+            questionTextView.layer.cornerRadius = 15.0
+            answerTextView.layer.cornerRadius = 15.0
+            
         }
     }
     
@@ -47,12 +57,46 @@ class FlashCardsViewController: UIViewController {
         
         if isAnswerViewFaceUp {
               UIView.transition(from: answerView, to: questionView, duration: 1.0, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: nil)
+            
+                questionAnswerLabel.text = "Question"
         } else {
             
             UIView.transition(from: questionView, to: answerView, duration: 1.0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+            
+                questionAnswerLabel.text = "Answer"
+
         }
       
     }
+    @IBAction func leftArrowButtonTapped(_ sender: UIButton) {
+        
+        if count == 0 {
+            return
+        }else {
+        count -= 1
+        questionTextView.text = flashCards![count].question
+        answerTextView.text = flashCards![count].answer
+        }
+    }
+    
+    
+    @IBAction func rightArrowButtonTapped(_ sender: UIButton) {
+        //go to next flashCard
+        
+        
+        if count + 1 == flashCards?.count {
+            return
+        } else {
+        count += 1
+        if let flashCard = flashCards?[count] {
+            questionTextView.text = flashCard.question
+            answerTextView.text = flashCard.answer
+        }
+    }
+}
+    
+    
+   
     //MARK: - Functions
     
     //Pass flashCard object to the next view controller
